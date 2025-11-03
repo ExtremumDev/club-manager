e7e05b3c0b767ba913c3a23cf5bee020b2714693
Vladislav <vladislav@jarvis.localdomain>
2025-11-03 05:05:22 +0000

    Bug in texts.py

--- filters/chat_filters.py
+++ filters/chat_filters.py
@@ -8,6 +8,8 @@ class GroupFilter:
 
     def __call__(self, event: types.CallbackQuery | types.Message):
         if isinstance(event, types.CallbackQuery):
+            
             return event.message.chat.id == chat_settings.GROUP_ID
         else:
+            print(event.chat.id)
             return event.chat.id == chat_settings.GROUP_ID

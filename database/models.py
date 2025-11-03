54db06f49f6e1e190127c08fc41b01c06640d142
Vladislav <vladislav@jarvis.localdomain>
2025-11-03 05:38:45 +0000

    Bug in models

--- database/models.py
+++ database/models.py
@@ -29,9 +29,9 @@ class Base(AsyncAttrs, DeclarativeBase):
 
         return ''.join(table_name) + 's'
 
-        def model_to_dict(model_instance):
-            return {column.name: getattr(model_instance, column.name) 
-                    for column in model_instance.__table__.columns}
+    def model_to_dict(model_instance):
+        return {column.name: getattr(model_instance, column.name) 
+                for column in model_instance.__table__.columns}
 
 
 

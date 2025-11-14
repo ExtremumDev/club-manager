256696d5c903fdf91344cd673078582508afb0b8
Extremum <vlados8118@gmail.com>
2025-11-14 20:29:13 +1000

    Redits fsm storage

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage
+
+from redis import Redis
 
 from config import settings
 
-
 bot = Bot(settings.BOT_TOKEN)
-dp = Dispatcher(storage=MemoryStorage())
+dp = Dispatcher(storage=RedisStorage(Redis()))

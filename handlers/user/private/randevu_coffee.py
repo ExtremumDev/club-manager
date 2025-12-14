956e35e64da334393766462c3aff3a0e8e73ab13
Extremum <vlados8118@gmail.com>
2025-12-14 20:00:26 +1000

    Randevu upgrade

--- handlers/user/private/randevu_coffee.py
+++ handlers/user/private/randevu_coffee.py
@@ -15,18 +15,20 @@ from utils.paging.user_events_paging import UserEventsPaging
 
 from utils.enums import EventType
 
+def get_randevu_settings_text(randevu_notifications: bool):
+    if randevu_notifications:
+        return "У вас включено получение предложений о встрече. Чтобы выключить👇"
+    else:
+        return "У вас сейчас выключено получение предложений о встрече. Чтобы включить👇"
+
 
 @connection
 async def send_coffe_info(c: types.CallbackQuery, db_session, *args):
     user = await UserDAO.get_obj(db_session, telegram_id=c.from_user.id)
-    if user.randevu_notifications:
-        text = "У вас включено получение предложений о встрече. Чтобы выключить👇"
-    else:
-        text = "У вас сейчас выключено получение предложений о встрече. Чтобы включить👇"
 
     await c.answer()
     await c.message.answer(
-        text=text,
+        text=get_randevu_settings_text(user.randevu_notifications),
         reply_markup=get_randevu_manage_markup(user.randevu_notifications)
     )
 
@@ -45,7 +47,12 @@ async def switch_mode(c: types.CallbackQuery, db_session, *args):
     else: # turn off
         text = "Вы не больше получите предложения о встречах, пока не включите их обратно"
 
-
+    await c.message.edit_text(
+        text=get_randevu_settings_text(new_mode)
+    )
+    await c.message.edit_reply_markup(
+        reply_markup=get_randevu_manage_markup(new_mode)
+    )
     await c.answer(
         text=text,
         show_alert=True
